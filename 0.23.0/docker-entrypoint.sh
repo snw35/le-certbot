18f9de22a54bd3ae278176072f9cd454dd75d7f2
snw35 <snw35@use.startmail.com>
2018-04-25 12:32:22 +0100

    Add support for multiple domains Revise readme Improve variable error checking Add openssl for cert checking

--- 0.23.0/docker-entrypoint.sh
+++ 0.23.0/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -uex
 
 # If we are passed arg flags then run against certbot certonly like an entrypoint
 if [ "${1:0:1}" = '-' ]; then
@@ -11,68 +11,98 @@ if [ "$1" = 'crond' ]; then
 
     # Get env vars
     if [ -z "${LE_EMAIL:-}" ]; then
-        echo "LE_EMAIL environment variable is not set! Defaulting to example@example.com"
-        declare -r LE_EMAIL='example@example.com'
+        echo "LE_EMAIL environment variable is not set! Please set this to an an email address you control."
+        exit 1
     fi
 
     if [ -z "${LE_TEST:-}" ]; then
         declare -r LE_TEST='n'
     fi
 
-    if [ -z "${LE_DOMAIN:-}" ]; then
-        echo "LE_DOMAIN environment variable is not set!"
-        echo "WARNING: certificates cannot be generated as domain is required!"
-        echo "Checking for existing certs..."
+    # Get all domain ENV vars
+    ENVDomains=`env | sort | awk -F "=" '{print $1}' | grep "LE_DOMAIN_.*"`
 
-        subdircount=`find /etc/letsencrypt/live/ -maxdepth 1 -type d | wc -l`
+    # Get all domains that certbot has certificates for in this volume
+    CertBotDomains=`certbot -n certificates | grep "Domains:"`
 
-        if [ $subdircount -ge 1 ]
-        then
-            echo "Existing certificate directory(s) found. Running crond."
-            exec "$@"
-        else
-            echo "ERROR: no exising certificate directory(s) found and LE_DOMAIN is not set!"
+    # Make sure there is at least one domain ENV var set
+    if [ -z "${ENVDomains:-}" ]; then
+        echo "WARNING: no LE_DOMAIN_* variables found! Certificates cannot be generated!"
+        echo "Checking for existing certificates..."
+
+        # If not, and certbot doesn't have any certs either, then exit as no action is possible
+        if [ -z "${CertBotDomains:-}" ]; then
+            echo "ERROR: no exising certificate(s) found and LE_DOMAIN_* is not set!"
             echo "Exiting as no certificats can be generated and there are none to renew!"
+            echo "Please check the LE_DOMAIN_* environment variables and configure for your domain(s)."
             exit 1
-        fi
-    else
-        # Certificate location
-        privkey="/etc/letsencrypt/live/$LE_DOMAIN/privkey.pem"
-
-        # Check for existing certificates
-        if [ -f "$privkey" ]; then
-            # If found, run crond
-            exec "$@"
+        # If not, but certbot has existing certs, then just run crond
         else
-            # If not, try to generate certs with the config given
-            echo "No existing certificates found for the configured domain. Attempting to generate them..."
+            echo "Existing certificate(s) found. Running crond."
+            exec "$@"
+        fi
+    # If domain ENV vars exist, loop through them
+    else
+        echo "$ENVDomains" | while IFS= read -r DomainVar ; do
 
-            if [ "$LE_TEST" = "n" ]; then
-                certbot certonly --webroot -w /usr/share/nginx/html -d $LE_DOMAIN --email $LE_EMAIL --agree-tos --non-interactive --keep-until-expiring --rsa-key-size 4096 --hsts --uir --preferred-challenges http
-
-                if [ $? -eq 0 ]; then
-                    echo "Certificates generated. Pausing and running crond..."
-                    sleep 2
-                    exec "$@"
-                else
-                    echo "Error generating certificates!"
-                    exit 1
-                fi
-            elif [ "$LE_TEST" = "y" ]; then
-                echo "Test certificate requested. Generating test cert instead..."
-                certbot certonly --webroot -w /usr/share/nginx/html -d $LE_DOMAIN --email $LE_EMAIL --agree-tos --non-interactive --keep-until-expiring --rsa-key-size 4096 --hsts --uir --preferred-challenges http --test-cert
-
-                if [ $? -eq 0 ]; then
-                    echo "Certificates generated. Pausing and running crond..."
-                    sleep 2
-                    exec "$@"
-                else
-                    echo "Error generating certificates!"
-                    exit 1
-                fi
+            # If current ENV var is empty, just move on
+            if [ -z "${!DomainVar}" ]; then
+                echo "$DomainVar is not set, continuing..."
+                continue
+            # If not empty, check to see if certbot already has a certificate for the domain
             else
-                echo "You must set LE_TEST to y if you want to generate a test certificate."
+                case "$CertBotDomains" in
+                    *"${!DomainVar}"* )
+                        # If certbot already has a cert for this domain, move on
+                        echo "Found existing certificate for ${!DomainVar}, continuing..."
+                        continue
+                    ;;
+                    * )
+                        # If not, try to generate certs with the config given
+                        echo "No existing certificate found for ${!DomainVar}, attempting to generate one..."
+
+                        # Generate normal certificate
+                        if [ "$LE_TEST" = "n" ]; then
+                            certbot certonly --webroot -w /usr/share/nginx/html -d "${!DomainVar}" --email "$LE_EMAIL"--agree-tos --non-interactive --keep-until-expiring --rsa-key-size 4096 --hsts --uir --preferred-challenges http
+
+                            echo "Certificate generated. Pausing and continuing..."
+                            sleep 2
+                            continue
+
+                        # Generate test certificate
+                        elif [ "$LE_TEST" = "y" ]; then
+                            echo "NOTE: TEST certificate(s) requested. Generating test certificate instead..."
+                            certbot certonly --webroot -w /usr/share/nginx/html -d "${!DomainVar}" --email "$LE_EMAIL" --agree-tos --non-interactive --keep-until-expiring --rsa-key-size 4096 --hsts --uir --preferred-challenges http --test-cert
+
+                            echo "Certificate generated. Pausing and continuing..."
+                            sleep 2
+                            continue
+
+                        # If LE_TEST was set to something else, exit as we don't know what the user wants
+                        else
+                            echo "LE_TEST has been set to: $LE_TEST"
+                            echo "You must set LE_TEST to 'y' if you want to generate a test certificate, or 'n' to generate a valid one!"
+                            echo "LE_TEST defaults to 'n' if unset."
+                            echo "Exiting"
+                            exit 1
+                        fi
+                    ;;
+                esac
             fi
+        done
+        # Re-check cerbot domains to make sure there is now at least one certificate
+        sleep 2
+        CertBotDomains=`certbot -n certificates | grep "Domains:"`
+        if [ -z "${CertBotDomains:-}" ]; then
+            echo "ERROR: no certificates were generated!"
+            echo "Exiting as there are no cerficates to renew!"
+            echo "Please check the LE_DOMAIN_* environment variables and configure for your domain(s)."
+            exit 1
+        # If there is now at least one certificate known to certbot, we can run crond
+        else
+            echo "Existing certificates detected."
+            echo "Running crond."
+            exec "$@"
         fi
     fi
 fi
--- README.md
+++ README.md
@@ -2,36 +2,29 @@
 
 Cerbot docker container based on Alpine Linux.
 
-This container will automate the **secure** deployment of Certbot in way that does not involve bind mounting the docker socket file into the container, or rely on any container that does.
-
-I wrote this because I couldn't find a method of using letsencrypt with docker that was secure enough for production deployment. [JrCs' letsencrypt nginx proxy](https://github.com/JrCs/docker-letsencrypt-nginx-proxy-companion) may be convenient and popular, but I don't want the docker socket bind-mounted into any of my production containers, ever. I also don't want to use simp_le or other 3rd party letsencrypt tools - I want to use the official one developed by the EFF, and that means Certbot.
+This container will automate the generation of letsencrypt certificates using certbot, and will run 'certbot renew' with crond to automatically renew them when they are close to expiring.
 
 ## How to use
 
-This image can be used in two modes:
+This image can be used in two main ways:
 
- 1. __Webroot__ (recommended): automatically generate certificates through a companion webserver, and then run crond with 'certbot renew' once certificates exist to automatically renew them with the webroot method. See [le-docker](https://github.com/snw35/le-docker) for full docker-compose file.
+### With a companion webserver
 
- 1. __Standalone__: run 'cerbot certonly' interactively for manual certificate generation, and then crond with 'certbot renew' to automatically renew them with the standalone method.
+Please see my [le-docker](https://github.com/snw35/le-docker) repository for the docker-compose file that will automatically deploy this image alongside my [le-nginx](https://github.com/snw35/le-nginx) proxy container.
 
-
-### Webroot
-
-Please see my [le-docker](https://github.com/snw35/le-docker) repository for the docker-compose file that will automatically deploy this image alongside my le-nginx proxy container.
-
-The advantage this will give you is that nginx can be configured to redirect all traffic to port 443 while still letting through the letsencrypt challenge, so you will have working SSL redirection.
+The advantage this will give you is that [le-nginx](https://github.com/snw35/le-nginx) has been configured to redirect all traffic to port 443 while still allowing through the letsencrypt challenges, so you will have working port 80 -> 443 redirection.
 
 ### Standalone
 
-To use the 'certbot certonly' command, run this image with direct arguments like an entrypoint:
+This container runs 'certbot certonly' as its default entry-point. You can pass arguments directly to it, or simply run it like this:
 ```
-docker run -it --mount source=le-certs,target=/etc/letsencrypt -p 80:80 snw35/le-certbot --standalone
+docker run -it --mount source=le-certs,target=/etc/letsencrypt -p 80:80 -p 443:443 snw35/le-certbot --standalone
 ```
-This will allow you to interactively obtain certificates and will store them inside the le-certs named volume. If you then run the container with the volume mounted, it will detect your existing certificates and revert to running crond with 'certbot renew':
+This will allow you to interactively obtain certificates and store them inside the le-certs named volume. If you then run the container with the volume mounted, it will detect your existing certificates and revert to running crond with 'certbot renew':
 ```
-docker run -it --mount source=le-certs,target=/etc/letsencrypt -p 80:80 snw35/le-certbot
+docker run -it --mount source=le-certs,target=/etc/letsencrypt -p 80:80 -p 443:443 snw35/le-certbot
 ```
-You can then mount the volume into your own containers to use the certificates at /etc/letsencrypt/(your domain)/live.
+You can then mount the le-certs volume into your own containers to use the certificates at /etc/letsencrypt/(your domain)/live.
 
 ***
 
